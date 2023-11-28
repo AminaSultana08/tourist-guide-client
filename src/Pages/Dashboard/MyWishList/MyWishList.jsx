61a081430edcfa0cd87fb0e5be64e1f9fdc5d849
AminaSultana08 <aminasultana088@gmail.com>
2023-11-28 19:08:01 +0600

    get data by tanstack query and added delete operation at wish list route od dashboard

--- src/Pages/Dashboard/MyWishList/MyWishList.jsx
+++ src/Pages/Dashboard/MyWishList/MyWishList.jsx
@@ -2,11 +2,44 @@
 import useWishList from "../../../hooks/useWishList";
 import { Link } from "react-router-dom";
 import { RiDeleteBin2Fill } from "react-icons/ri";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const MyWishList = () => {
-    const [wishList] = useWishList()
-    const totalPrice = wishList.reduce((total, item) => total + item.price, 0)
+    const [wishList, refetch] = useWishList()
+    const axiosSecure = useAxiosSecure()
+    const totalPrice = wishList.reduce((total, item) => total + item.price, 0);
+    const handleDelete = id =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+
+            if (result.isConfirmed) {
+            
+            axiosSecure.delete(`/wishList/${id}`)
+            .then(res=>{
+                if(res.data.deletedCount>0){
+                    refetch()
+                      Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                  });
+    
+                }
+            } )
+           
+            }
+          });
+        
+    }
     return (
         <div className="bg-red-100 min-h-screen ">
             <div className="flex justify-evenly py-7">
@@ -21,8 +54,8 @@ const MyWishList = () => {
                 
             </div>
             {/* Table */}
-            <div className="overflow-x-auto bg-red-100 min-h-screen">
-  <table className="table w-full ml-5">
+            <div className="overflow-x-auto bg-red-100 min-h-screen mr-5">
+  <table className="table w-full mx-5 ">
     {/* head */}
     <thead className="bg-red-200   ">
       <tr className="py-10">
@@ -35,6 +68,7 @@ const MyWishList = () => {
         <th>Name</th>
         <th >Tour Type</th>
         <th >Price</th>
+        <th>Details </th>
         <th>Action </th>
       </tr>
     </thead>
@@ -66,16 +100,8 @@ const MyWishList = () => {
             </td>
             <td className="pr-2">{item?.price}tk </td>
 
-            <th>
-            <button
-        
-        className="middle none center rounded-lg bg-gradient-to-tr mx-4 from-red-700 to-red-500 py-2 px-4 font-sans text-lg font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      >
-        < RiDeleteBin2Fill className="text-lg " />
-      </button>
-      
-            </th>
             
+
             <th>
             <Link to={`/packageDetails/${item._id}`}>    <button
             data-ripple-light="true"
@@ -84,6 +110,17 @@ const MyWishList = () => {
           >
            View Details
           </button></Link>
+            </th>
+
+            <th>
+            <button
+            onClick={()=>handleDelete(item._id)}
+        
+        className="middle none center rounded-lg bg-gradient-to-tr mx-4 from-red-700 to-red-500 py-2 px-4 font-sans text-lg font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      >
+        < RiDeleteBin2Fill className="text-lg " />
+      </button>
+      
             </th>
            
           </tr>)
